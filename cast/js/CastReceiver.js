c3e8176ffee99ad43e168fd39f37e9cf5dce4b42
Tomasz Kwiatkowski <quiath@gmail.com>
2020-05-07 01:20:54 +0200

    Rearrange fps code

--- cast/js/CastReceiver.js
+++ cast/js/CastReceiver.js
@@ -163,7 +163,8 @@ if (true) {
             skavg = 0.9 * skavg + 0.1 * skipped;
             //let at = playerManager.getAbsoluteTimeForMediaTime(ct);
             //document.getElementById("mytext").innerHTML = `at: ${at} ct: ${ct}`;
-            document.getElementById("mytext").innerHTML = `ct: ${ct} fps.avg: ${avg > 0 ? 1 / avg : 0} s.avg: ${skavg}`;
+            let fps = (avg > 0) ? 1000 / avg : 0;
+            document.getElementById("mytext").innerHTML = `ct: ${ct} fps.avg: ${fps} s.avg: ${skavg}`;
 
         }
         window.requestAnimationFrame(onUpdate);
